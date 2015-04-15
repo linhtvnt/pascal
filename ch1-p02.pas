af83de1ad746f2a6b4d3d1829c40b002d7f8fc95
Tran Van Linh <linhtvnt.tc@gmail.com>
2015-04-15 17:59:52 +0700

    Create ch1-p02.pas

---
+++ ch1-p02.pas
@@ -0,0 +1,12 @@
+{ Chuong trinh nhap vao 2 so nguyen bat ky; tinh tong, hieu cua 2 so do }
+program ch1_p02;
+var a,b: integer;
+begin
+   write('Hay nhap so thu nhat:');
+   readln(a);
+   write('Hay nhap so thu hai:');
+   readln(b);
+   writeln('Tong cua 2 so la:',a+b);
+   writeln('Hieu cua 2 so la:',a-b);
+   readln;
+end.
